dac1a91bf8f7bc946f7687c369399a0ef2650ed5
Daniil Zaburunov <zaburunov.2000@mail.ru>
2024-07-17 01:44:37 +0400

    создвл папку с двумя компонентами

--- src/App.js
+++ src/App.js
@@ -1,55 +1,10 @@
 import React from "react";
-
+import { Posts } from "./components/Posts";
 export default class App extends React.Component {
-  state = {
-    count: 0,
-    isCounting: false,
-  };
-
-  componentDidMount() {
-    const userCount = localStorage.getItem("timer");
-    if (userCount) {
-      this.setState({ count: +userCount });
-    }
-  }
-
-  componentDidUpdate() {
-    localStorage.setItem("timer", this.state.count);
-  }
-
-  componentWillUnmount() {
-    clearInterval(this.couterId);
-  }
-
-  handleStart = () => {
-    this.setState({ isCounting: true });
-
-    this.couterId = setInterval(() => {
-      this.setState({ count: this.state.count + 1 });
-    }, 1000);
-  };
-
-  handleStop = () => {
-    this.setState({ isCounting: false });
-    clearInterval(this.couterId);
-  };
-
-  handleReset = () => {
-    this.setState({ isCounting: false, count: 0 });
-    clearInterval(this.couterId);
-  };
-
   render() {
     return (
       <div className="App">
-        <h1>React Timer</h1>
-        <h3>{this.state.count}</h3>
-        {!this.state.isCounting ? (
-          <button onClick={this.handleStart}>Start</button>
-        ) : (
-          <button onClick={this.handleStop}>Stop</button>
-        )}
-        <button onClick={this.handleReset}>Reset</button>
+        <Posts posts={this.state.posts} />
       </div>
     );
   }
