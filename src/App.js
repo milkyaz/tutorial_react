7ba440cb26624beb0a219f1b2ed65ccc2b70036a
Daniil Zaburunov <zaburunov.2000@mail.ru>
2024-07-16 23:57:40 +0400

    Создал таймер по курсу "React для современных веб-приложений"

--- src/App.js
+++ src/App.js
@@ -1,25 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends React.Component {
+  state = {
+    count: 0,
+    isCounting: false,
+  };
+
+  componentDidMount() {
+    const userCount = localStorage.getItem("timer");
+    if (userCount) {
+      this.setState({ count: +userCount });
+    }
+  }
+
+  componentDidUpdate() {
+    localStorage.setItem("timer", this.state.count);
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.couterId);
+  }
+
+  handleStart = () => {
+    this.setState({ isCounting: true });
+
+    this.couterId = setInterval(() => {
+      this.setState({ count: this.state.count + 1 });
+    }, 1000);
+  };
+
+  handleStop = () => {
+    this.setState({ isCounting: false });
+    clearInterval(this.couterId);
+  };
+
+  handleReset = () => {
+    this.setState({ isCounting: false, count: 0 });
+    clearInterval(this.couterId);
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <h1>React Timer</h1>
+        <h3>{this.state.count}</h3>
+        {!this.state.isCounting ? (
+          <button onClick={this.handleStart}>Start</button>
+        ) : (
+          <button onClick={this.handleStop}>Stop</button>
+        )}
+        <button onClick={this.handleReset}>Reset</button>
+      </div>
+    );
+  }
 }
-
-export default App;
