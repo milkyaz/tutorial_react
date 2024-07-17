e573b5a090046c490e80a073188053b3cde1fa10
Daniil Zaburunov <zaburunov.2000@mail.ru>
2024-07-17 16:13:33 +0400

    Добавил коммиты для усвоения, изученного материала

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { Posts } from "./components/Posts";
 import React, { Component } from "react";
 
 class App extends Component {
+  //Вот этот state с тремя записями мы передаем в Posts
   state = {
     posts: [
       { id: "abc1", name: "JS Basics" },
@@ -14,11 +15,12 @@ class App extends Component {
     console.log("App.jsx setState update");
   };
 
+  //Предыдущий коммент(//Вот этот state с тремя записями мы передаем в Posts). Передаём вот сюда вниз..
   render() {
     const { posts } = this.state;
     return (
       <div className="App">
-        <Posts posts={posts} cb={this.handleSomething} />
+       <Posts posts={posts} cb={this.handleSomething} />
       </div>
     );
   }
--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,4 +1,10 @@
 export function Post(props) {
   const { name, cb } = props;
-  return <h2 onClick={cb}>{name}</h2>;
+  //и здесть выводится на страничку браузера! после трех этапов. App.js -> Posts.jsx -> Post.jsx
+  return (
+    <div>
+      <h2 onClick={cb}>{name}</h2>
+      <button>delete</button>
+    </div>
+  );
 }
