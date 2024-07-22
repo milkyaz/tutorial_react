40847c60394d669bbc20fb087af4a8c04e125b2d
Daniil Zaburunov <zaburunov.2000@mail.ru>
2024-07-22 16:14:57 +0400

    Работа с формами в React. Управляемые компоненты

--- src/App.js
+++ src/App.js
@@ -1,41 +1,10 @@
-import { Posts } from "./components/Posts";
-import { Gallery } from "./components/Gallery";
-import React, { Component } from "react";
+import { Form } from "./components/Form";
 
-class App extends Component {
-  //Вот этот state с тремя записями мы передаем в Posts
-  state = {
-    posts: [
-      { id: "my-bsc", name: "JS Basics" },
-      { id: "my-adv", name: "JS Advanced" },
-      { id: "my-rct", name: "React JS" },
-    ],
-  };
-  /* С помощью этой функции передаем id и фильтруем. Для обновления состояния используем setState.   */
-  deleteElem = (id) => {
-    const posts = this.state.posts;
-    console.log(id);
-    this.setState({ posts: posts.filter((post) => post.id !== id) });
-    // const posts = this.state.posts;
-    //удаляет элемент с полседнеего индекса
-    // if (posts.length > 0) {
-    //   const lastIndex = posts.length - 1;
-    //   this.setState({
-    //     posts: posts.filter((posts, index) => index !== lastIndex),
-    //   });
-    // }
-    // console.log(posts);
-  };
-  render() {
-    const { posts } = this.state;
-
-    return (
-      <div className="App">
-        <Gallery />
-        <Posts posts={posts} onDelete={this.deleteElem} />
-      </div>
-    );
-  }
+function App() {
+  return (
+    <div className="App">
+      <Form />
+    </div>
+  );
 }
-
 export default App;
---
+++ src/components/Form.jsx
@@ -0,0 +1,37 @@
+import React from "react";
+
+class Form extends React.Component {
+  state = {
+    firstName: "",
+    email: "",
+  };
+  //с помощью event я могу достать некое значение
+  handleChange = (event) => {
+    //Можно использовать одну и ту же функцию для разных секций, а ключ использовать динамический([event.target.name])
+    this.setState({ [event.target.name]: event.target.value });
+  };
+
+  render() {
+    const { firstName, email } = this.state;
+    return (
+      <div>
+        <input
+          type="text"
+          name="firstName"
+          placeholder="firstName"
+          value={firstName}
+          onChange={this.handleChange}
+        />
+        <input
+          type="email"
+          name="email"
+          placeholder="email"
+          value={email}
+          onChange={this.handleChange}
+        />
+      </div>
+    );
+  }
+}
+
+export { Form };
