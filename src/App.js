24a0e47a8df8c8188271f843584c6296e6a061aa
Daniil Zaburunov <zaburunov.2000@mail.ru>
2024-07-27 19:15:13 +0400

    очистил лишнее от предыдущего проекта, добавил код по теме "Работа с состоянием. Функциональный react"

--- src/App.js
+++ src/App.js
@@ -1,41 +1,18 @@
-import { Posts } from "./components/Posts";
-import { Gallery } from "./components/Gallery";
-import React, { Component } from "react";
+import React from "react";
+import { useState } from "react";
+function App() {
+  const [count, setCount] = useState(0);
+  const [count2, setCount2] = useState(false);
 
-class App extends Component {
-  //Вот этот state с тремя записями мы передаем в Posts
-  state = {
-    posts: [
-      { id: "my-bsc", name: "JS Basics" },
-      { id: "my-adv", name: "JS Advanced" },
-      { id: "my-rct", name: "React JS" },
-    ],
-  };
-  /* С помощью этой функции передаем id и фильтруем. Для обновления состояния используем setState.   */
-  deleteElem = (id) => {
-    const posts = this.state.posts;
-    console.log(id);
-    this.setState({ posts: posts.filter((post) => post.id !== id) });
-    // const posts = this.state.posts;
-    //удаляет элемент с полседнеего индекса
-    // if (posts.length > 0) {
-    //   const lastIndex = posts.length - 1;
-    //   this.setState({
-    //     posts: posts.filter((posts, index) => index !== lastIndex),
-    //   });
-    // }
-    // console.log(posts);
-  };
-  render() {
-    const { posts } = this.state;
-
-    return (
-      <div className="App">
-        <Gallery />
-        <Posts posts={posts} onDelete={this.deleteElem} />
-      </div>
-    );
+  const increment = () => {
+    setCount(count + 1)
   }
+
+  return (
+    <div className="App">
+      <button onClick={increment}>{count}</button>
+    </div>
+  );
 }
 
 export default App;
