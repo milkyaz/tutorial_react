2cf88b421fdbcade65c3215dde493613027323d3
Daniil Zaburunov <zaburunov.2000@mail.ru>
2024-07-27 20:30:27 +0400

    использование Ref

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
 import { useState, useEffect } from "react";
-import Clicker from "./components/Clicker";
+// import Clicker from "./components/Clicker";
+import WithRef from "./components/Ref";
 
 function App() {
   //состояние, видим мы кликер на странице или нет(фолс - не видим, тру - видим)
-  const [isClicker, setClicker] = useState(false);
+  // const [isClicker, setClicker] = useState(false);
   return (
     <div className="App">
       <h2>React App</h2>
+      <WithRef />
       {/* кнопка, которая переключает состояние с фолс, на тру */}
-      <button onClick={() => setClicker(!isClicker)}>Toggle clicer</button>
-      {isClicker && <Clicker />}
+      {/* <button onClick={() => setClicker(!isClicker)}>Toggle clicer</button>
+      {isClicker && <Clicker />} */}
     </div>
   );
 }
---
+++ src/components/Ref.jsx
@@ -0,0 +1,30 @@
+import { useEffect, useRef, useState } from "react";
+
+const WithRef = () => {
+  //   const inputEl = useRef(null);
+  //   console.log(inputEl);
+  const numRef = useRef(0);
+  const [count, setCount] = useState(0);
+
+  const handleClick = () => {
+    // numRef.current = 1;
+  
+    setCount((prevCount) => prevCount + 1);
+    setCount((prevCount) => prevCount + 1);
+    setCount((prevCount) => prevCount + 1);
+  };
+
+  //   useEffect(() => {
+  //     // console.log(inputEl);
+  //     console.log(numRef.current);
+  //   });
+
+  return (
+    <div>
+      {/* <input placeholder="name" ref={inputEl} /> */}
+      <button onClick={handleClick}>{count}</button>
+    </div>
+  );
+};
+
+export default WithRef;
